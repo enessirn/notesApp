4be00f02df06edcb3cb1d387f2ddea98c7c8b685
Enes Sirin <sefsirin55@gmail.com>
2023-03-30 12:25:03 +0300

    Toplu Değişimler | Dark Mode olayları | data İndex | kategori filtreleme

--- script.js
+++ script.js
@@ -44,21 +44,33 @@ close.addEventListener("click",()=>{
 
 
 
-var allCards = []
+var allCards;
+var categoryCounter;
 
-var categoryCounter = 0
-
-
-
+if (localStorage.getItem('cards')) {
+    allCards = JSON.parse(localStorage.getItem('cards'))
+    categoryCounter = allCards.length
+}
+else {
+    allCards = []
+    categoryCounter = allCards.length
+}
+ if (allCards.length > 0) {
+    for (let i = 0; i < allCards.length; i++){
+        categoryIcerikEkle(allCards[i].name,allCards[i].desc,allCards[i].tag)
+    }
+ }
 function yeniIcerikEkle(name,desc,tag){
     let card = document.createElement("div")
     card.classList.add("card")
     card.classList.add("animate__animated")
     card.classList.add("animate__zoomInDown")
+    card.dataset.index = categoryCounter
     content.appendChild(card)
 
     let cardHeader = document.createElement("div")
     cardHeader.classList.add("card-header")
+    cardHeader.style.textTransform = 'capitalize'
     cardHeader.innerHTML = `${name}  <i class='fa-solid fa-trash'></i>`
     card.appendChild(cardHeader)
 
@@ -68,6 +80,7 @@ function yeniIcerikEkle(name,desc,tag){
 
     let cardDesc = document.createElement("div")
     cardDesc.classList.add("card-desc")
+    cardDesc.style.textTransform = 'capitalize'
     cardDesc.textContent = desc
     card.appendChild(cardDesc)
 
@@ -80,23 +93,26 @@ function yeniIcerikEkle(name,desc,tag){
     allCards[categoryCounter] = {
         "name" : name,
         "desc" : desc,
-        "tag"  : tag
+        "tag"  : tag,
+        "index" : categoryCounter
     }
     categoryCounter++
     localStorage.setItem('cards',JSON.stringify(allCards))
 
 }
 
-function categoryIcerikEkle(name,desc,tag) {
+function categoryIcerikEkle(name,desc,tag,index) {
     let card = document.createElement("div")
     card.classList.add("card")
     card.classList.add("animate__animated")
     card.classList.add("animate__zoomInDown")
+    card.dataset.index = index
     content.appendChild(card)
 
     let cardHeader = document.createElement("div")
     cardHeader.classList.add("card-header")
     cardHeader.innerHTML = `${name}  <i class='fa-solid fa-trash'></i>`
+   
     card.appendChild(cardHeader)
 
     let clearDiv = document.createElement("div")
@@ -114,8 +130,8 @@ function categoryIcerikEkle(name,desc,tag) {
     cardTag.textContent = tag
     card.appendChild(cardTag)
 
-}
 
+}
 formKaydet.addEventListener("click",()=> {
     
     if ((notEkleBaslik.value).trim() != "" && (notEkleTextArea.value).trim() != "" && selectValue.value != "null") {
@@ -137,7 +153,7 @@ formKaydet.addEventListener("click",()=> {
 
 function cardRemove(event) {
     const eventTarget = event.target;
-   
+    
     if(eventTarget.classList[1] == "fa-trash"){
         const cardElement  = eventTarget.parentElement.parentElement
         let removeConfirm = confirm("R u sure?")
@@ -149,6 +165,7 @@ function cardRemove(event) {
                 popUp.classList.remove("pop-up-active")
              
              },2000)
+             console.log(eventTarget)
              cardElement.remove()
             
             
@@ -158,62 +175,52 @@ function cardRemove(event) {
 }
 addEventListener("click",cardRemove)
 
-
-
-
-
 function darkMode() {
     document.body.classList.toggle("dark-bg")
     darkButton.classList.toggle("light-bg")
     lightBtn.classList.toggle("d-none")
     darkBtn.classList.toggle("d-none")
     footer.classList.toggle("text-white")
-  
     if (document.body.classList[0] == "dark-bg") {
         localStorage.setItem("dark","true")
     }
     else {
         localStorage.setItem("dark","false")
     }
+
+
 }
 
 darkButton.addEventListener("click",darkMode)
 
-// function darkModeControl() {
-//     if(localStorage.getItem("dark") == "true"){
-//      document.body.classList.add("dark-bg")
-//      darkButton.classList.add("light-bg")
-//      lightBtn.classList.add("d-none")
-//      darkBtn.classList.remove("d-none")
-//      footer.classList.add("text-white")
-//     }
-//     else {
-//      document.body.classList.remove("dark-bg")
-//      darkButton.classList.remove("light-bg")
-//      lightBtn.classList.add("d-none")
-//      darkBtn.classList.remove("d-none")
-//      footer.classList.remove("text-white")
-//     }
-//   }
-// darkModeControl()
+function darkModeControl() {
+    if (localStorage.getItem('dark') === 'true') {
+        darkButton.click()
+    }
+}
 
-
-
+darkModeControl()
 
 
 //? Category Map
 
 function categorySelect(nameCategory) {
-    if (allCards.length > 0) {
-        content.innerHTML = "";
-        allCards.filter((card) => {
-            return card.tag === nameCategory;
-        }).map((card) => {
-            categoryIcerikEkle(card.name, card.desc, card.tag);
-        });
+        if (allCards.length > 0) {
+            content.innerHTML = "";
+            allCards.filter((card) => {
+                return card.tag === nameCategory;
+            }).map((card) => {
+                categoryIcerikEkle(card.name, card.desc, card.tag);
+              
+            });
+        }
     }
-}
+  
 
+const allCardsCategoryButton = document.querySelector('#allCategoryButton')
+allCardsCategoryButton.addEventListener('click',()=>{
+    location.reload()
+})
 categoryButtons.forEach((button) => {
     button.addEventListener("click", () => {
 
@@ -226,4 +233,5 @@ categoryButtons.forEach((button) => {
       categorySelect(button.dataset.category);
     });
   });
+
 
